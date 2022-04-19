3e2c5b75f28f94b3706a3144868e108867d177a8
Yiming <ybian@iastate.edu>
2022-04-19 00:28:29 -0500

    finish some tools

---
+++ Dataset_and_Tools/Construction_detail_done.py
@@ -0,0 +1,52 @@
+import os
+'''
+    For the given path, get the List of all files in the directory tree 
+'''
+def getListOfFiles(dirName):
+    # create a list of file and sub directories 
+    # names in the given directory 
+    listOfFile = os.listdir(dirName)
+    allFiles = list()
+    # Iterate over all the entries
+    for entry in listOfFile:
+        # Create full path
+        fullPath = os.path.join(dirName, entry)
+        # If entry is a directory then get the list of files in this directory 
+        if os.path.isdir(fullPath):
+            allFiles = allFiles + getListOfFiles(fullPath)
+        else:
+            allFiles.append(fullPath)
+                
+    return allFiles    
+
+
+def Get_dataset_construction(directory, keys, keywords, pct = False):
+    allFiles = getListOfFiles(directory)
+    construction = {}
+    for i in range(len(keys)):
+        counter = 0
+        for f in allFiles:
+            if keywords[i] in f:
+                counter = counter + 1
+        construction[keys[i]] = counter
+    if pct:
+        pct_list = list()
+        tt = sum(construction.values())
+        for i in range(len(construction)):
+            pct_list.append(round(construction[keys[i]]/tt*100,2))
+            pass
+        return construction,pct_list
+    else:
+        return construction
+
+
+
+keys = ["pythonfiles", "matlabfiles"]
+keywords = ['.py', '.m']
+DIR = './'
+
+A,B = Get_dataset_construction(DIR,keys,keywords,True)
+#A = Get_dataset_construction(DIR,keys,keywords)
+
+print(A)
+print(B)
---
+++ Dataset_and_Tools/SetCreator_done.py
@@ -0,0 +1,70 @@
+import shutil, os
+import random
+
+
+def Create_train_val_data(src_mother_dir, src_folder, dest_mother_dir, dest_folder, set_name, total_num, ratio = 2/3, WindowsOS = False):
+    for i in range(len(src_folder)):
+        src = os.path.join(src_mother_dir,src_folder[i],set_name)
+        src_files = os.listdir(src)
+        dest_train_dir = os.path.join(dest_mother_dir,set_name,"train",dest_folder[i])
+        dest_val_dir = os.path.join(dest_mother_dir,set_name,"val",dest_folder[i])
+
+        if WindowsOS:
+            src = src.replace('/','\\')
+            dest_train_dir = dest_train_dir.replace('/','\\')
+            dest_val_dir = dest_val_dir.replace('/','\\')
+
+        if not os.path.exists(dest_train_dir):
+            os.makedirs(dest_train_dir)
+            print(f'folder created: {os.path.join("train",dest_folder[i])} \n')
+        if not os.path.exists(dest_val_dir):
+            os.makedirs(dest_val_dir)
+            print(f'folder created: {os.path.join("val",dest_folder[i])} \n')
+
+        train_num = int(total_num*ratio)
+
+        rand_idx = random.sample(range(0, len(src_files)), total_num)
+
+        for i in range(total_num):
+            full_name = os.path.join(src, src_files[rand_idx[i]])
+            if i<train_num:
+                shutil.copy(full_name, dest_train_dir)
+            else:
+                shutil.copy(full_name, dest_val_dir)     
+
+def Create_test_data(src_mother_dir, src_folder, dest_mother_dir, dest_folder, set_name , num_per_cate, WindowsOS = False):
+    for i in range(len(src_folder)):
+        src = os.path.join(src_mother_dir,src_folder[i],set_name)
+        src_files = os.listdir(src)
+        dest_test_dir = os.path.join(dest_mother_dir,set_name,"test",dest_folder[i])
+
+        if WindowsOS:
+            src = src.replace('/','\\')
+            dest_train_dir = dest_train_dir.replace('/','\\')
+            dest_val_dir = dest_val_dir.replace('/','\\')
+
+        if not os.path.exists(dest_test_dir):
+            os.makedirs(dest_test_dir)
+            print(f'folder created: {os.path.join("test",dest_folder[i])} \n')
+
+        rand_idx = random.sample(range(0, len(src_files)), num_per_cate)
+
+        for i in range(num_per_cate):
+            full_name = os.path.join(src, src_files[rand_idx[i]])
+            shutil.copy(full_name, dest_test_dir)
+
+
+
+source_folders = ["n01531178(goldfinch)","n02342885(hamster)","n03400231(frying pan)","n03950228(pitcher)","n04515003(upright)"]
+src_mother_dir = "D:/Academic/2022Spring/575/Project/Model/PIC_generator"
+
+dest_mother_dir = "D:/Academic/2022Spring/575/Project/Model/PIC_generator/data"
+dest_folders = ["goldfinch","hamster","frying pan","pitcher","upright"]
+
+total_num = 300
+
+Create_train_val_data(  src_mother_dir=src_mother_dir, src_folder=source_folders, dest_mother_dir=dest_mother_dir,
+                        dest_folder=dest_folders, set_name="GS", total_num=total_num, WindowsOS=True)
+
+Create_test_data(   src_mother_dir=src_mother_dir, src_folder=source_folders, dest_mother_dir=dest_mother_dir,
+                    dest_folder=dest_folders, set_name="GS", num_per_cate=200, WindowsOS=False)
